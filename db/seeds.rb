3f1dbdd554be669533d992b4428da6bfe7e50936
brianchz <brybcook@gmail.com>
2023-08-02 10:44:11 -0500

    created user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+
+User.create!([{
+        summoner_name: 'Cheezey',
+        roles: ['Mid', 'Support', 'Jungle'],
+        rank: 'Platinum',
+        seriousness_level: 'Flexible'
+    },
+    {
+        summoner_name: 'DaCooksta',
+        roles: ['Top', 'Jungle', 'ADC'],
+        rank: 'Silver',
+        seriousness_level: 'Focused'
+    },
+    {
+        summoner_name: 'Pillowslaya',
+        roles: ['Bot', 'Support', 'Jungle'],
+        rank: 'Silver',
+        seriousness_level: 'Flexible'
+    },
+    {
+        summoner_name: 'Eddyfahkre',
+        roles: ['Top', 'Support'],
+        rank: 'Platinum',
+        seriousness_level: 'Flexible'
+    },
+    {
+        summoner_name: 'LoveOfTheGame',
+        roles: ['Support'],
+        rank: 'Silver',
+        seriousness_level: 'Casual' 
+    },
+    {
+        summoner_name: 'Nogabbagabba',
+        roles: ['Top'],
+        rank: 'Bronze',
+        seriousness_level: 'Casual'
+    },
+])
